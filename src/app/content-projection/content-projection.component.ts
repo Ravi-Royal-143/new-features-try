ec88254676b9f5e6167a3029f684c83a8f1a9f23
Ravikumar C S <raviroyalmech233@gmail.com>
2023-03-15 18:42:21 +0530

    added route

--- src/app/content-projection/content-projection.component.ts
+++ src/app/content-projection/content-projection.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-content-projection',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './content-projection.component.html',
   styleUrls: ['./content-projection.component.scss']
 })
