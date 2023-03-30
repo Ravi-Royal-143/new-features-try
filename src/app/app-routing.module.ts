ed57e6e25f376db7ed269916af4a5d7b819c966e
Ravikumar C S <raviroyalmech233@gmail.com>
2023-03-30 14:33:03 +0530

    added default ro routes page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,11 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'material'
+    redirectTo: 'routes'
+  },
+  {
+    path: '**',
+    redirectTo: 'routes'
   }
 ];
 
