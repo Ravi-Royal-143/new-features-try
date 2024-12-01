8dffc2526514e6eb6f61cb022e3aca7a12af37c8
Ravi-Royal-143 <81796854+Ravi-Royal-143@users.noreply.github.com>
2024-12-01 20:10:07 +0530

    Add linked signal component with temperature conversion functionality as example

---
+++ src/app/signal/linked-signal/linked-signal.component.ts
@@ -0,0 +1,20 @@
+import { Component, linkedSignal, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-linked-signal',
+  imports: [FormsModule],
+  templateUrl: './linked-signal.component.html',
+  styleUrl: './linked-signal.component.scss'
+})
+export class LinkedSignalComponent {
+
+  celsius = signal<number>(0);
+
+  fahrenheit = linkedSignal({
+    source: this.celsius,
+    computation: (celsius) => { 
+      return celsius * 9 / 5 + 32;
+    }
+  })
+}
--- src/app/signal/signal.routing.ts
+++ src/app/signal/signal.routing.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { SignalComponent } from "./signal.component";
 import { BasicSignalComponent } from "./basic-signal/basic-signal.component";
 import { SignalObservableComponent } from "./signal-observable/signal-observable.component";
+import { LinkedSignalComponent } from "./linked-signal/linked-signal.component";
 
 export const SIGNAL_ROUTES: Routes = [{
     path: '',
@@ -18,6 +19,10 @@ export const SIGNAL_ROUTES: Routes = [{
         {
             path: 'signal-observable',
             component: SignalObservableComponent
+        },
+        {
+            path: 'linked-signal',
+            component: LinkedSignalComponent
         }
     ]
 }];
