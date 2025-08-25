cebdd3158a43506d66561886130ffa6d45c8afcd
Ravi-Royal-143 <81796854+Ravi-Royal-143@users.noreply.github.com>
2025-08-25 11:59:19 +0530

    feat: enhance layout with responsive utilities and improve component structures

--- src/app/material/material.component.ts
+++ src/app/material/material.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-material',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatIconModule],
   templateUrl: './material.component.html',
   styleUrls: ['./material.component.scss'],
 })
